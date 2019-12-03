2732965a80dd65d49eb07b521c178013b25711be
Aaron Bae <aaronbae@uw.edu>
2019-12-03 04:43:47 -0800

    finished simulated annealing and more

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,6 +2,10 @@
 
 string Utils::GENES = "012345678"; //9abcdefghijklmno";    // Valid Genes
 
+void Utils::seed()
+{
+  srand(time(nullptr));
+}
 int Utils::randomGenerator(int num)
 {
     return (int) floor((rand()%(num)));
@@ -17,4 +21,65 @@ char Utils::mutated_genes()
   int len = GENES.size();
   int r = random_num(0, len-1);
   return GENES[r];
+}
+int Utils::fitness(Sudoku s)
+{
+  int size = s.size;
+  int count = 0,cost;
+  bool flag;
+  int i,j;
+  for(int k=0;k<size;k++)
+  {
+    for (i=0;i<size;i++)
+    {
+      flag = false;
+      cost =0;
+      for (j = 0; j < size; j++)
+      {
+        if (s.board[i][j] == k)
+        {
+          flag = true;
+          cost++;
+        }
+      }
+
+      if(flag == false)
+      {
+        cost++;
+      }
+      else
+      {
+        cost--;
+      }
+      count = cost + count;
+    }
+  }
+
+  cost = 0;
+  for(int k=0;k<size;k++) 
+  {
+    for (i=0;i<size;i++) 
+    {
+      flag = false;
+      cost=0;
+      for (j = 0; j < size; j++) 
+      {
+        if (s.board[j][i] == k) 
+        {
+          flag = true;
+          cost++;
+        }
+      }
+      if(flag == false)
+      {
+        cost++;
+      }
+      else
+      {
+        cost--;
+      }
+      count = cost + count;
+    }
+  }
+  return count;
 }
